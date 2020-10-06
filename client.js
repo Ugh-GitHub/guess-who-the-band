4cdb440bd8ec7a2a087bc0b6301ed35bc6b7e6d9
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2020-10-06 09:27:14 -0500

    base functionality completed

--- client.js
+++ client.js
@@ -1,25 +1,50 @@
 console.log('Here are all the available people:', people);
 
 $(document).ready(loadUp);
+let pick = randomNumber(0,people.length-1);
 
 
 function loadUp() {
     console.log('Hello Jquery');
-    let pick = randomNumber(1,people.length);
+    
     // console.log(pick);
     divBuilder(people);
+
+    $('#prompt').empty();
+    $('#buildSite').on('click','.gamePiece',guessChecker);
+    
 }
 
+
+
 function divBuilder(objArray) {
-    console.log("in divAppend");
-    objArray.forEach(element => {
-        // console.log(element.githubUsername)
-        $('#buildSite').append(`<div class='gamePiece'
-        data-name='${element.name}' 
-        data-githubUsername='${element.githubUsername}'>Some Text</div>`)
-    });
+    console.log("in divBuilder");
+    for (let i = 0; i < objArray.length; i++) {
+        $(buildSite).append(`<div>
+        <input type="image" data-id=${[i]} data-name=${objArray[i].name} src="Images/${objArray[i].githubUsername}.jpeg" class="gamePiece" />
+        </div>`);
+        
+    }
+    // objArray.forEach(element => {
+    //     // console.log(element.githubUsername)
+        // $(buildSite).append(`<div data-id=${element}>
+        // <input type="image" src="Images/${element.githubUsername}.jpeg" name="guessWho" class="gamePiece" />
+        // </div>`)
+    // });
 }
 
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function guessChecker() {
+    console.log('in guessChecker');
+    console.log($(this).data('id'));
+    if (pick == $(this).data('id')) {
+        window.alert("You got it!!!");
+        pick = randomNumber(0,people.length-1);
+    }
+    else {
+    window.alert("try again");
+    }
 }
