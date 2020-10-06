5db4b24aaae9f260cdc8384a5575f9b4e06ff7c4
Peter Ugh-GitHub <peterp.lyle@gmail.com>
2020-10-06 11:55:29 -0500

    finalized subminssion

--- client.js
+++ client.js
@@ -14,22 +14,13 @@ function loadUp() {
     
 }
 
-
-
 function divBuilder(objArray) {
     console.log("in divBuilder");
     for (let i = 0; i < objArray.length; i++) {
-        $(buildSite).append(`<div>
+        $(buildSite).append(`
         <input type="image" data-id=${[i]} data-name=${objArray[i].name} src="Images/${objArray[i].githubUsername}.jpeg" class="gamePiece" />
-        </div>`);
-        
+        `); 
     }
-    // objArray.forEach(element => {
-    //     // console.log(element.githubUsername)
-        // $(buildSite).append(`<div data-id=${element}>
-        // <input type="image" src="Images/${element.githubUsername}.jpeg" name="guessWho" class="gamePiece" />
-        // </div>`)
-    // });
 }
 
 function pickRandomPerson() {
@@ -44,16 +35,13 @@ function randomNumber(min, max){
 }
 
 function guessChecker() {
-    console.log('in guessChecker');
-    console.log($(this).data(''));
+    // console.log('in guessChecker');
+    // console.log($(this).data(''));
     if ($('#prompt').data('pick') == $(this).data('id')) {
         window.alert("You got it!!!");
         pickRandomPerson();
-        // pick = randomNumber(0,people.length-1);
-        // $('#prompt').empty();
-        // $('#prompt').append('Here are the guesses: ',people[pick].name);
     }
     else {
-    window.alert("try again");
+        window.alert("try again");
     }
 }
